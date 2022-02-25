f713b942cb9a1a9043b957e57a90c59c5769818c
takoson68 <95896770+takoson68@users.noreply.github.com>
2022-02-25 10:49:01 +0800

    加入血壓列表

--- js/blood_list.js
+++ js/blood_list.js
@@ -18,7 +18,12 @@ var mdBoxApp = new Vue({
       //   "high": null
       // }]
     },
-    newData: [],
+    pickMe: 1,
+    newData: {
+      heartbeat: 0,
+      low: 0,
+      high: 0
+    },
     firebaseConfig: {
       apiKey: "AIzaSyAiUpK-r_z4wlGN4asKiyQXtutipf7kr1Y",
       authDomain: "bloodlist-38979.firebaseapp.com",
@@ -26,7 +31,9 @@ var mdBoxApp = new Vue({
       storageBucket: "bloodlist-38979.appspot.com",
       messagingSenderId: "697182918191",
       appId: "1:697182918191:web:bfb3339590fec0af2f58da"
-    }
+    },
+    openAddBox: true,
+    keyboardBox: ['1','2','3','4','5','6','7','8','9','<','0','enter'],
   },
   created: function () {
     this.getDataBox()
@@ -75,28 +82,21 @@ var mdBoxApp = new Vue({
       });
 
     },
+    addList(){
+      this.dataBox.list = [...this.dataBox.list,{
+          time: new Date(),
+          heartbeat: 33,
+          low: 88,
+          high: 123
+        }]
+        this.updateData()
+    },
+    delList(j){
+      this.dataBox.list.splice(j, 1);
+      this.updateData()
+    },
     updateData(){
-      let dd = {
-          "name": "林小頭",
-          'id': 'pData',
-          "list": [{
-            "time": {
-              "seconds": 1645545600,
-              "nanoseconds": 0
-            },
-            "heartbeat": 65,
-            "low": 88,
-            "high": 125
-          },{
-            "time": {
-              "seconds": 1645545600,
-              "nanoseconds": 0
-            },
-            "heartbeat": 55,
-            "low": 98,
-            "high": 115
-          }]
-        }
+      let dd = this.dataBox
       this.ref.update(dd).then(() => {
         console.log('add data successful');
         this.getDataBox()
@@ -130,6 +130,71 @@ var mdBoxApp = new Vue({
         this.allName = []
         this.getAllName()
       });
+    },
+    convertTime(time){
+      // console.log(new Date(time*1000).toLocaleString());
+      // let DD = {
+      //   y: new Date(time*1000).getFullYear(),
+      //   m: (new Date(time*1000).getMonth()*1)+1,
+      //   d: new Date(time*1000).getDate(),
+      //   t: new Date(time*1000).getHours(),
+      // }
+      // return DD
+      return new Date(time*1000).toLocaleString()
+    },
+    openAddListBox(){
+      this.pickMe = 1
+      this.openAddBox = true
+    },
+    cloMe(){
+      this.pickMe = 1
+      this.openAddBox = false
+      this.newData = {
+        heartbeat: 0,
+        low: 0,
+        high: 0
+      }
+    },
+    pickMeBox(e){
+      this.pickMe = e
+    },
+    addNumber(ele){
+      console.log(ele);
+      if(ele=='enter'){
+        this.newData.time= new Date()
+        console.log(this.newData);
+        this.dataBox.list = [...this.dataBox.list,this.newData]
+        this.updateData()
+        this.openAddBox = false
+        this.newData = {
+          heartbeat: 0,
+          low: 0,
+          high: 0
+        }
+      }
+
+      let dd = this.pickMe==1?'low':this.pickMe==2?'high':this.pickMe==3?'heartbeat':''
+      if(dd==''){return false}
+      if(ele=='<'){
+        this.newData[dd] = (this.newData[dd]+'').replace(/.$/, '')
+        if(this.newData[dd]<=0){
+          this.newData[dd] = 0
+        }
+      }else{
+        if(this.newData[dd]*1>=100){
+          this.pickMe++
+          return false
+        }
+        this.newData[dd] = (this.newData[dd]*1 + ele)*1
+      }
+      if(this.newData[dd]>=250){
+        this.pickMe++
+        if(this.pickMe>=4){
+          this.pickMe=1
+        }
+      }
+
+
     },
   },
 });
